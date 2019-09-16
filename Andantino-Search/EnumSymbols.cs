be19db262639c01ef49a1dd41f3343ece5afead2
Ismail Alaoui Abdellaoui <60974@aui.ma>
2019-09-17 01:35:50 +0200

    removed unnecessary packages

--- Andantino-Search/EnumSymbols.cs
+++ Andantino-Search/EnumSymbols.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Andantino_Search
+﻿namespace Andantino_Search
 {
     public enum EnumSymbols
     {
